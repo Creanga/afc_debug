57a55f0eebf3bf667c0eaf5140d80690c2b98b1c
Anatoly Creanga <creanga@yandex.ru>
2017-05-25 19:30:33 +0300

    Remove function debug_env
    Bugs fixes
    The debug_env task is solved using the 'debug' function

--- afc/afc_debug.php
+++ afc/afc_debug.php
@@ -1,6 +1,4 @@
-<?php // ver. 2017-01-19
-
-define('MAX_STAGE', 4); // Мах function level to debug
+<?php // v1.01 2017-01-26 +
 function debug() {
 	$d = debug_backtrace();
 	// print_r($d);
@@ -11,17 +9,21 @@ function debug() {
 	for ($i=$b; $i < $c; $i++)
 		if (!in_array($d[$i]["function"], $a))
 			break;
-	echo '<script type="text/javascript">', PHP_EOL;
-	if ($b > 0) {
-		$f = " [function]=>{$d[$i]['function']}()";
-		for ($j=$i + 1; $j < min($i + MAX_STAGE, $c); $j++)
-			$f .= " <- {$d[$j]['function']}()";
+	if ($i >= $c) {
+		$b = 0;
+		$i = $c - 1;
+	}
+	echo '<script type="text/javascript">' . PHP_EOL;
+	if ($b) {
+		$f = " [function]=>" . $d[$i]['function'] . "()";
+		for ($j=$i + 1; $j < min($i + 3, $c); $j++)
+			$f .= " <- " . $d[$j]['function'] . "()";
 		if ($j < $c)
 			$f .= " ...";
 	} else
 		$f = '';
-	$ret = "[file]=>{$d[$i - $b]['file']} [line]=>{$d[$i - $b]['line']} $f";
-	echo 'console.debug("' . str_replace('\\', '\\\\', $ret) . '");', PHP_EOL;
+	$ret = '[file]=>' . $d[$i - $b]['file'] . ' [line]=>' . $d[$i - $b]['line'] . $f;
+	echo 'console.debug("' . str_replace('\\', '\\\\', $ret) . '");' . PHP_EOL;
 	$n = func_num_args();
 	for ($i = 0; $i < $n; $i++) {
 		$a1 = func_get_arg($i);
@@ -31,18 +33,18 @@ function debug() {
 		} else
 			$a2 = '';
 		$a1 = '  ' . json_encode($a1);
-		echo "console.debug('  ', " . $a1 . $a2 . ");", PHP_EOL;
+		echo "console.debug('  ', " . $a1 . $a2 . ");" . PHP_EOL;
 	}
-	echo "</script>", PHP_EOL;
+	echo "</script>" . PHP_EOL;
 	return $ret;
 }
 
-function debug_stop($value = null, $stop_condition = true, $print_to_screen = true, $only_on_stop = true) {
+function debug_stop($value = null, $print_to_screen = true, $stop_condition = true, $only_on_stop = true) {
 	if (!$stop_condition && $only_on_stop) {
 		return;
 	}
 	if (is_null($value))
-		$bp = debug('');
+		$bp = debug();
 	else {
 		$bp = debug('$value', $value);
 		if ($print_to_screen) {
@@ -56,25 +58,5 @@ function debug_stop($value = null, $stop_condition = true, $print_to_screen = tr
 	}
 	if ($stop_condition)
 		die("Останов: $bp");
-}
-function debug_env() {
-	$d = debug_backtrace();
-	$c = count($d);
-	$b = $c >> 1 ? 1 : 0;
-	$i = $b;
-	echo '<script type="text/javascript">', PHP_EOL;
-	if ($b > 0) {
-		$f = " [function]=>{$d[$i]['function']}()";
-		for ($j=$i + 1; $j < min($i + MAX_STAGE, $c); $j++)
-			$f .= " <- " . $d[$j]['function'] . "()";
-		if ($j < $c)
-			$f .= " ...";
-	} else
-		$f = '';
-	$ret = '[file]=>' . $d[$i - $b]['file'] . ' [line]=>' . $d[$i - $b]['line'] . $f;
-	echo 'console.debug("' . str_replace('\\', '\\\\', $ret) . '");', PHP_EOL;
-	foreach ($_SERVER as $a => $v)
-		echo "console.debug('   $a => $v');", PHP_EOL;
-	echo "</script>", PHP_EOL;
 }
 ?>
