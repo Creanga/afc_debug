55749655fac5351ab7464a615768331f5552b2d1
Anatoly Creanga <creanga@yandex.ru>
2017-05-26 09:31:55 +0300

    Now you can use $depth as param in 'debug' function

--- afc/afc_debug.php
+++ afc/afc_debug.php
@@ -1,34 +1,45 @@
-<?php // v1.02 2017-03-18
+<?php // v1.03 2017-03-18
 
-function GetBacktrace($depth) {
-    $s = '';
+function GetBacktrace($depth)
+{
+    // $s = '';
     $MAXSTRLEN = 64;
     $trace_array = debug_backtrace();
-    while (in_array($trace_array[1]['function'] ?? '', array('debug', 'debug_stop')))
+    while (in_array($trace_array[1]['function'] ?? '', ['debug', 'debug_stop']))
         array_shift($trace_array);
-    $line = (isset ($trace_array[0]['line']) ? $trace_array[0]['line']: 'unknown');
-    $file = (isset ($trace_array[0]['file']) ? basename($trace_array[0]['file']): 'unknown');
-    $s .= sprintf('# %s # %s:%d', $trace_array[0]['function'], $file, $line);
+    $file = $trace_array[0]['file'] ?? 'unknown';
+    $line = $trace_array[0]['line'] ?? 'unknown';
+    $a[] = sprintf('# %s(%d)', $file, $line);
     array_shift($trace_array);
-    $pops = sizeof($trace_array) - $depth;
-    for ($i = 0; $i < $pops; $i++)
-        array_pop($trace_array);
+
+    // If a depth is specified, we leave only the first depth values in the array
+    if ($depth > 0) {
+        $pops = sizeof($trace_array) - $depth;
+        for ($i = 0; $i < $pops; $i++)
+            array_pop($trace_array);
+    } else
+        $pops = 0;
     $tabs = sizeof($trace_array);
     foreach ($trace_array as $arr) {
         if ($tabs)
-            $s .= " <- ";
+            $s = '# ';
         $tabs -= 1;
-        if (isset ($arr['class']))
-            $s .= $arr['class'] . '.';
+        $line = ($arr['line'] ?? 'unknown');
+        $file = (isset ($arr['file']) ? $arr['file']: 'unknown');
+        $s .= sprintf('%s(%d)::', $file, $line);
+        $s .= $arr['class'] ?? '';
+        $s .= $arr['type'] ?? '';
         $args = array ();
         if (!empty ($arr['args']))
             foreach ($arr['args'] as $v) {
                 if (is_null($v))
                     $args[] = 'null';
                 elseif (is_array($v))
-                    $args[] = 'Array[' . sizeof($v) . ']';
+                    // $args[] = 'Array[' . sizeof($v) . ']';
+                    $args[] = json_encode($v);
                 elseif (is_object($v))
-                    $args[] = 'Object: ' . get_class($v);
+                    // $args[] = 'Object:' . get_class($v);
+                    $args[] = json_encode($v);
                 elseif (is_bool($v))
                     $args[] = $v ? 'true' : 'false';
                 else {
@@ -40,54 +51,54 @@ function GetBacktrace($depth) {
                 }
             }
         $s .= $arr['function'] . '(' . implode(', ', $args) . ')';
-        $line = (isset ($arr['line']) ? $arr['line']: 'unknown');
-        $file = (isset ($arr['file']) ? basename($arr['file']): 'unknown');
-        $s .= sprintf(':%s:%d', $file, $line);
+        $a[] = $s;
     }
     if ($pops > 0)
-        $s .= ' ...';
-    return $s;
+        $a[] = '# ...';
+    return $a;
 }
 
-function debug() {
+function debug(array $params = null, $depth = 0)
+{
     echo '<script type="text/javascript">' . PHP_EOL;
 
-    $ret = GetBacktrace(3);
-
-    echo 'console.debug("' . str_replace('\\', '\\\\', $ret) . '");' . PHP_EOL;
-    $n = func_num_args();
-    for ($i = 0; $i < $n; $i++) {
-        $a1 = func_get_arg($i);
-        if (is_string($a1) && strlen($a1) > 0 && $a1[0] === '$' && $i + 1 < $n) {
-            $i++;
-            $a2 = ", '->', " . json_encode(func_get_arg($i));
-        } else
-            $a2 = '';
-        $a1 = '  ' . json_encode($a1);
-        echo "console.debug('  ', $a1$a2);" . PHP_EOL;
+    $a = GetBacktrace($depth);
+    $a = array_reverse($a);
+    $s = '';
+    foreach ($a as $v) {
+        $s .= str_replace(['\\', "'", "\n", "\r"], ['\\\\', "\\'", '.', '.'], $v) . '\n';
+    }
+    echo "console.debug('$s');" . PHP_EOL;
+    foreach ($params as $key => $value) {
+        $v = '  ' . json_encode($value);
+        echo "console.debug('  $key: ', $v);" . PHP_EOL;
     }
     echo "</script>" . PHP_EOL;
-    return $ret;
+    return $a;
 }
 
-function debug_stop($value = null, $print_to_screen = true, $stop_condition = true, $only_on_stop = true) {
+function debug_stop($value = null, $print_to_screen = true, $stop_condition = true, $only_on_stop = true)
+{
     if (!$stop_condition && $only_on_stop) {
         return;
     }
     if (is_null($value))
         $bp = debug();
-    else {
+    else
         $bp = debug('$value', $value);
-        if ($print_to_screen) {
-            echo "value = ";
-            if (is_string($value)) {
+    if ($print_to_screen) {
+        // $bp = array_reverse($bp);
+        $s = '';
+        foreach ($bp as $v)
+            $s .= $v . '<br />';
+        if ($value) {
+            if (is_string($value))
                 $value = htmlspecialchars($value);
-            }
-            print_r($value);
-            echo '<br />';
+            $s .= "value = " . print_r($value, true) . '<br />';
         }
+        echo $s;
     }
     if ($stop_condition)
-        die("Останов: $bp");
+        die("Останов программы");
 }
 ?>
